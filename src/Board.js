cf7864368f16d7a2730798a79f2d65254e7734b9
KyleSeckman <kseckm1@gmail.com>
2022-09-18 18:55:49 -0400

    flipping cell

--- src/Board.js
+++ src/Board.js
@@ -63,6 +63,7 @@ class Board extends Component {
   /** handle changing a cell: update board & determine if winner */
 
   flipCellsAround(coord) {
+    console.log('flipping', coord)
     let {ncols, nrows} = this.props;
     let board = this.state.board;
     let [y, x] = coord.split("-").map(Number);
@@ -76,12 +77,19 @@ class Board extends Component {
       }
     }
 
+    flipCell(y,x);
+    flipCell(y, x -1)
+    flipCell(y, x +1)
+    flipCell(y - 1, x)
+    flipCell(y + 1, x)
+
     // TODO: flip this cell and the cells around it
 
     // win when every cell is turned off
     // TODO: determine is the game has been won
+    let hasWon=false;
 
-    // this.setState({board, hasWon});
+    this.setState({board, hasWon});
   }
 
 
@@ -94,7 +102,7 @@ class Board extends Component {
       let row = [];
       for( let x = 0; x <this.props.ncols; x++) {
         let coord = `${y}-${x}`
-        row.push(<Cell key={coord} isLit={this.state.board[y][x]} />)
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)} />)
       }
       tblBoard.push(<tr key={y} >{row}</tr>);
     }
